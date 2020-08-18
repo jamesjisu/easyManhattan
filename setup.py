28474efa5de18282b78ff0a3c1596a0f2e9b7bc6
James Han <jamesjisu@vpn172022149060.its.yale.internal>
2020-08-18 15:47:45 +0900

    Adding files

---
+++ setup.py
@@ -0,0 +1,22 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="easyManhattan",
+    version="0.0.1",
+    author="James Han",
+    author_email="jamesjisu@gmail.com",
+    description="An easy-to-use package to generate Manhattan plots",
+    long_description=An easy-to-use package to generate a diverse range of Manhattan plots.,
+    long_description_content_type="text/markdown",
+    url="https://github.com/jamesjisu/easyManhattan",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.6',
+)
